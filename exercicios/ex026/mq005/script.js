d456d8c8ff09399fe00e7aa053d43ebd1219c7cc
Rebeca Kethelyn Lopes <104172440+RebecaKLopes@users.noreply.github.com>
2022-12-08 23:47:16 -0300

    criação de menu para desktop

--- exercicios/ex026/mq005/script.js
+++ exercicios/ex026/mq005/script.js
@@ -5,3 +5,11 @@ function clickMenu(){
         menu.style.display = 'block'
     }
 }
+
+function mudouTamanho(){
+    if(window.innerWidth >= 768){
+        menu.style.display = 'block'
+    } else {
+        menu.style.display = 'none'
+    }
+}
